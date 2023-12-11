8bb2a2e08b22f5a5fa8cd7d2136b4b73db7a40ab
Ethan Doherty <ethandorts2002@gmail.com>
2023-12-11 22:53:59 +0000

    Code added

---
+++ app/src/main/java/com/example/textdetectiondec2023/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.textdetectiondec2023;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button btnProcess;
+    TextView txtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.quote);
+        txtView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void processText(View view) {
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(), R.drawable.quote
+        );
+        TextRecognizer recognizer =
+                TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        InputImage image = InputImage.fromBitmap(myBitmap, 0);
+        recognizer.process(image)
+                .addOnSuccessListener(new OnSuccessListener<Text>() {
+                    @Override
+                    public void onSuccess(Text visionText) {
+                        String text = visionText.getText();
+                        txtView.setText(text);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+}
